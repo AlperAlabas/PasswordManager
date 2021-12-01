c7d158fb11bba52ae955a25eafb19bd0901b3330
Alper Alabaş <alper.alabas@ogr.sakarya.edu.tr>
2021-12-02 02:52:49 +0300

    change to native base

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import {
+  Input,
+  IconButton,
+  Checkbox,
+  Text,
+  Box,
+  VStack,
+  HStack,
+  Heading,
+  Icon,
+  Center,
+  NativeBaseProvider,
+} from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NativeBaseProvider>
+        <Center flex={1}>
+          <Text>Merhaba!</Text>
+        </Center>
+      </NativeBaseProvider>
   );
 }
 
