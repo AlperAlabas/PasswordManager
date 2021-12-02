7f147fafa4ea3bb30c1cf9af26d00fa81a10d03a
Alper Alabaş <alper.alabas@ogr.sakarya.edu.tr>
2021-12-02 03:39:49 +0300

    ver fight

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet} from 'react-native';
+
 import {
   Input,
   IconButton,
@@ -25,11 +25,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
